de0b9e87538fd6e7a4c82e81608eb59992cda52a
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-03 23:24:37 +0200

    feat (creazione parte statica del campo titolo,contenuto e check box )

--- src/Components/PostForm.jsx
+++ src/Components/PostForm.jsx
@@ -7,7 +7,7 @@ const PostForm =()=>{
     return(
         
         <div className="container">
-            <h1 className='mb-4'>Crea un nuovo Post</h1>
+            <h1 className='mb-4 text-center'>Crea un nuovo Post</h1>
            <form>
               {/* Campo Autore */}
              <div className="row mb-3">
@@ -22,7 +22,56 @@ const PostForm =()=>{
              
             />
           </div>
-        </div>        
+        </div>   
+         {/* Campo Titolo */}
+        <div className="row mb-3">
+          <div className="col">
+            <label htmlFor="title" className="form-label">Titolo</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              name="title"
+              placeholder="Inserisci il titolo del post"
+              
+            />
+          </div>
+        </div>
+        {/* Campo Contenuto */}
+        <div className="row mb-3">
+          <div className="col">
+            <label htmlFor="body" className="form-label">Contenuto</label>
+            <textarea
+              className="form-control"
+              id="body"
+              name="body"
+              rows="4"
+              placeholder="Scrivi il contenuto del post..."    
+            ></textarea>
+          </div>
+        </div> 
+        {/* Checkbox*/}
+        <div className="row mb-4">
+          <div className="col">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="isPublic"
+                name="isPublic"
+              />
+              <label className="form-check-label" htmlFor="isPublic">Rendi il post pubblico
+              </label>
+            </div>
+          </div>
+        </div>
+
+        {/* Pulsante di invio */}
+        <div className="row">
+          <div className="col">
+            <button type="submit" className="btn btn-primary">Invia</button>
+          </div>
+        </div>
       </form> 
      </div> 
    
